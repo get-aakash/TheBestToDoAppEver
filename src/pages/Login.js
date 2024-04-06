c6a35730feb932b3ff009cd5f3333d51a81659d1
Aakash Acharya <mail.aakash108@gmail.com>
2024-04-06 12:37:43 +1100

    sign up module

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,11 +2,18 @@ import React, { useEffect, useState } from 'react'
 import DefaultLayout from '../components/DefaultLayout'
 import { Button, Col, Form, Row } from 'react-bootstrap'
 import CustomInput from '../components/CustomInput'
-
+import { onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth'
+import { auth } from '../firebase-config/firebaseConfig'
+import { toast } from 'react-toastify'
+import { useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { setUser } from '../redux/userSlice'
 
 const Login = () => {
   const [formData, setFormData] = useState({})
-  
+  const dispatch = useDispatch()
+  const {userInfo} = useSelector(state=>state.user)
+  const navigate = useNavigate()
 
   const inputs = [{
     label: "Email",
@@ -24,11 +31,18 @@ const Login = () => {
 
   }]
 
+  onAuthStateChanged(auth, (user)=>{
+    user && dispatch(setUser(user))
+})
+  useEffect(()=>{
+    userInfo?.uid && navigate("/dashboard")
+  },[])
 
 
 
   const handleOnChange = (e) => {
     const { name, value } = e.target
+    setFormData({...formData, [name]: value})
 
 
   }
@@ -36,6 +50,25 @@ const Login = () => {
 
   const handleOnSubmit = async (e) => {
     e.preventDefault()
+    try {
+      const responsePending = signInWithEmailAndPassword(auth, formData.email, formData.password)
+      toast.promise(responsePending,{
+        pending: "Please wait..."
+      })
+      const {user} = await responsePending
+      if (user?.uid){
+        const userObj = {
+          uid: user.uid,
+          email: user.email,
+          displayName: user.displayName
+        }
+        dispatch(setUser(userObj))
+        return toast.success("User Logged in Successfully")
+        
+      }
+    } catch (error) {
+      
+    }
 
 
 
@@ -48,8 +81,8 @@ const Login = () => {
           <h3>Welcome back!!</h3>
           <hr />
 
+          {inputs.map((item, i) => <CustomInput key={i} {...item} onChange={handleOnChange} />)}
 
-          <CustomInput  onChange={handleOnChange} />
 
           <div className="d-grid mt-5">
             <Button type='submit' variant='success'>SigIn</Button>
